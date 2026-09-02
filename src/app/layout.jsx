a11aff565feb2f3c3a859247aa49abe42665b503
Amiraeone <amiraeone@gmail.com>
2026-09-02 09:06:30 +0330

    style:footer

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
     >
       <body className="flex min-h-full flex-col bg-white">
         <Navbar />
-        <main className="grow container mx-auto bg-orange-200/40">
+        <main className="grow container mx-auto">
           {children}
         </main>
         <Footer />
--- src/components/generals/Footer.jsx
+++ src/components/generals/Footer.jsx
@@ -5,28 +5,29 @@ export default function Footer() {
     return (
         <footer className='z-50 bg-white shadow border-t-2 space-y-4'>
             <div className='flex px-8 py-4'>
-                <div className='w-1/3 p-4 flex'>
+                <div className='w-1/3 p-4'>
                     <h2 className='text-2xl font-bold'>Dara</h2>
-                    <p className='p-2 mt-8'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, quae unde.</p>
+                    <p className='space-y-4 mt-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, quae unde.</p>
                 </div>
-                <nav className='w-1/3 p-4 flex'>
+                <nav className='w-1/3 p-4'>
                     <h3 className='text-xl font-bold'>navigate</h3>
-                    <ul className='p-2 mt-8 list-disc   '>
+                    <ul className='space-y-4 mt-4'>
                         <li><Link href={'/'} className='hover:text-blue-600'>Home</Link></li>
                         <li><Link href={'/products'} className='hover:text-blue-600'>Products</Link></li>
                         <li><Link href={'/blogs'} className='hover:text-blue-600'>Blogs</Link></li>
                     </ul>
                 </nav>
-                <div className='w-1/3 p-4 flex'>
+                <div className='w-1/3 p-4'>
                     <h3 className='text-xl font-bold'>trend blogs</h3>
-                    <ul className='p-2 mt-8 list-disc'>
+                    <ul className='space-y-4 mt-4'>
                         <li>firt blog</li>
                         <li>second blog</li>
                         <li>third blog</li>
                     </ul>
                 </div>
             </div>
-            <div className='text-xl text-center mx-auto my-4'>created by ❤️</div>
+            <hr />
+            <div className='text-lg text-center mx-auto my-4'>created by ❤️</div>
         </footer>
     )
 }
