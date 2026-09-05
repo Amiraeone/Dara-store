83545c7cefd96eaa7fd4e73e013602d5b9a581cc
Amiraeone <amiraeone@gmail.com>
2026-09-05 12:26:31 +0330

    refactor:checkout cart

--- src/app/(cart)/checkout/page.jsx
+++ src/app/(cart)/checkout/page.jsx
@@ -2,27 +2,45 @@
 
 import CheckoutCard from '@/components/card/CheckoutCard'
 import useCartStore from '@/store/cart-store'
+import { PackageOpen } from 'lucide-react'
 
 export default function Checkout() {
   const cart = useCartStore(s => s.cart)
 
+  if (cart.length === 0) {
+    return (
+      <section className='w-full h-80 flex justify-center items-center'>
+        <div className='flex flex-col justify-center items-center w-2/4 h-2/4 py-8 px-12 bg-secondary rounded-xl shadow'>
+          <PackageOpen className='text-gray-500 w-11 h-11' />
+          <h2 className='text-lg text-gray-500 font-bold'>
+            there is no item
+          </h2>
+        </div>
+      </section>
+    )
+  }
+
   return (
     <section className='w-full flex justify-around p-8'>
-      <div className='flex justify-between gap-8'>
-        <div>
+      <div className='flex justify-between gap-8 w-3/4 relative'>
+        <div className='flex-8'>
           <h2 className='text-xl font-bold'>Cart</h2>
-          <hr />
+          <hr className='my-4' />
           {cart.map((product) => (
             <CheckoutCard key={product.id} product={product} />
           ))}
         </div>
-        <div className='w-90 relative bg-secondary rounded-2xl p-4 space-y-2'>
+        <div className='flex-4 w-96 h-fit sticky top-26 bg-secondary rounded-2xl px-8 py-4 space-y-4'>
           <h2 className='text-xl font-bold'>Payment Ditails</h2>
           <hr />
-          <div className='flex flex-col justify-end'>
+          <div className='flex flex-col gap-4 mt-2'>
             <div className='flex justify-between'>
-              <span className='text-lg font-bold'>Total Price</span>
-              <span className='text-lg'>$50000</span>
+              <span className='text-md'>Total Products Price ({cart.length} product)</span>
+              <span className='text-md'>${cart.reduce((sum, current) => sum + current.price, 0)}</span>
+            </div>
+            <div className='flex justify-between'>
+              <span className='text-lg font-bold'>Total Cart Price</span>
+              <span className='text-lg font-bold'>${cart.reduce((sum, current) => sum + current.price, 0)}</span>
             </div>
             <button className='px-4 py-2 bg-blue-700 rounded-lg text-white hover:bg-blue-700/90 cursor-pointer'>Payment</button>
           </div>
--- src/components/card/CheckoutCard.jsx
+++ src/components/card/CheckoutCard.jsx
@@ -10,19 +10,17 @@ export default function CheckoutCard({ product }) {
     const removeFromCart = useCartStore(state => state.removeFromCart)
 
     return (
-        <div className='p-4 hover:bg-secondary rounded'>
-            <div className='flex gap-4 justify-between'>
-                <div className='flex gap-4'>
-                    <div className='flex justify-center items-center bg-secondary rounded-xl p-4 w-1/4'>
-                        <img className="w-20" src={product.image} />
-                    </div>
-                    <div>
-                        <h2 className='text-xl font-bold'>
-                            {product.title}
-                        </h2>
-                    </div>
+        <div className='p-4 hover:bg-secondary rounded duration-300 h-40'>
+            <div className='h-full flex gap-2 justify-between'>
+                <div className='flex-2 flex justify-center items-center bg-secondary rounded-xl p-4 w-1/4'>
+                    <img className="h-20" src={product.image} />
                 </div>
-                <div className='flex flex-col justify-between items-end'>
+                <div className='flex-5 ml-4'>
+                    <h2 className='text-xl font-bold'>
+                        {product.title}
+                    </h2>
+                </div>
+                <div className='flex-3 flex flex-col justify-between items-end'>
                     <span className='text-xl font-bold'>${product.price}</span>
                     <Popover open={open} onOpenChange={setOpen}>
                         <PopoverTrigger render={<Button variant="outline" size="icon" className='cursor-pointer h-8 w-8 flex justify-center items-center hover:bg-secondary shadow rounded-lg text-center'><Trash2 /></Button>} />
