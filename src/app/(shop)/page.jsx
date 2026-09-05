53025c9748d92e04965c56160a7a80722c1505dd
Amiraeone <amiraeone@gmail.com>
2026-09-05 09:05:14 +0330

    refactor:accordion component

--- src/app/(shop)/page.jsx
+++ src/app/(shop)/page.jsx
@@ -1,5 +1,5 @@
 import ArticleCard from "@/components/card/ArticleCard";
-import Accordion from "@/components/generals/Accordion"
+import Accordion from "@/components/Accordion"
 
 export const metadata = {
   title: "Dara | Home",
