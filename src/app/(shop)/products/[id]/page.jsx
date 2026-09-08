13a92bbba700f744a0b00aa6732a8714d95cabf9
Amiraeone <amiraeone@gmail.com>
2026-09-08 15:16:44 +0330

    feat:category page

--- src/app/(shop)/products/[id]/page.jsx
+++ src/app/(shop)/products/[id]/page.jsx
@@ -1,8 +1,15 @@
 import AddToCart from '@/components/AddToCart'
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbSeparator
+} from "@/components/ui/breadcrumb"
 
 export default async function page({ params }) {
     const { id } = await params
-    
+
     const res = await fetch(`https://fakestoreapi.com/products/${id}`)
     const product = await res.json()
 
@@ -12,6 +19,19 @@ export default async function page({ params }) {
                 <img className='h-80' src={product.image} />
             </div>
             <div className='flex-4 p-4 space-y-2'>
+                <Breadcrumb>
+                    <BreadcrumbList>
+                        <BreadcrumbItem>
+                            <BreadcrumbLink render={<a href="/products" />}>Product</BreadcrumbLink>
+                        </BreadcrumbItem>
+                        <BreadcrumbSeparator />
+                        <BreadcrumbItem>
+                            <BreadcrumbLink render={<a href={`/products?category=${product.category}`} />}>
+                                {product.category}
+                            </BreadcrumbLink>
+                        </BreadcrumbItem>
+                    </BreadcrumbList>
+                </Breadcrumb>
                 <h2 className='text-xl font-bold'>{product.title}</h2>
                 <p>{product.description}</p>
             </div>
@@ -19,6 +39,7 @@ export default async function page({ params }) {
                 <div className='bg-secondary p-4 rounded-2xl shadow flex flex-col gap-4'>
                     <span className='font-bold'>⭐ {product.rating.rate}</span>
                     <hr />
+                    <span>buy more than three from this product to give %10 discount</span>
                     <span className='font-bold text-xl'>${product.price}</span>
                     <AddToCart product={product}>Add To Cart</AddToCart>
                 </div>
--- src/app/(shop)/products/page.jsx
+++ src/app/(shop)/products/page.jsx
@@ -1,25 +1,29 @@
 import ProductCard from "@/components/card/ProductCard";
 
-export const metadata = {
-  title: "Dara | Products",
-  description: "Data Product Page",
-};
+export async function generateMetadata({ searchParams }) {
+  const { category } = await searchParams
+  return {
+    title: category ? `Dara | ${category} Products` : 'Dara | Products',
+    description: "Data Product Page",
+  }
+}
 
-export default async function Products() {
-  const res = await fetch('https://fakestoreapi.com/products' , {
+export default async function Products({ searchParams }) {
+  const { category } = await searchParams
+
+  const res = await fetch('https://fakestoreapi.com/products', {
     next: {
       revalidate: 5
     }
   })
   const products = await res.json()
+  const categoryProducts = category && products.filter(item => item.category === category)
 
-  return (
-    <div>
-      <section className="flex gap-4 p-8 flex-wrap">
-        {products.map(product => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-      </section>
-    </div>
-  )
+  return <section className="flex gap-4 p-8 flex-wrap">
+    {category ? categoryProducts.map(product => (
+      <ProductCard key={product.id} product={product} />
+    )) : products.map(product => (
+      <ProductCard key={product.id} product={product} />
+    ))}
+  </section>
 }
